3ca3957d792519684d0f14b27b6cb3fb23737a71
unknown <erbynn1234@gmail.com>
2023-05-19 08:25:59 +0000

    hmm

--- client/zip-recuiter/src/index.tsx
+++ client/zip-recuiter/src/index.tsx
@@ -13,13 +13,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AuthProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </AuthProvider>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
